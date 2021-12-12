6f4cb31756358e3084cd04eee76f52f215d8c5b4
Christian Bayquen <cgabayquen@gmail.com>
2021-12-13 05:06:39 +0800

    add: update event

---
+++ api/get.php
@@ -0,0 +1,41 @@
+<?php
+    // get all events organizer is a part of
+    require_once '../config/database.php';
+    require_once '../util/auth.php';
+    
+    // header('Access-Control-Allow-Origin: *');
+    // header('Content-Type: application/json');
+    // header('Access-Control-Allow-Methods: GET');
+    // header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization,X-Requested-With');
+    
+    // validate request
+    $headers = apache_request_headers();
+    if (!checkKey($headers)) {
+        echo "Invalid API key";
+        return;
+    }
+
+    $action = $_GET["action"];
+
+    switch ($action) {
+        case "GET_EVENTS_BY":
+            getEventsBy();
+            return;
+    }
+
+    function getEventsBy() {
+        $userId = json_decode($_GET["user_id"]);
+        
+        $database = new Database();
+        $db = $database->connect();
+    
+        $statement = $db->prepare("SELECT * from event inner join admin_events on 
+                event.event_id = admin_events.event_id where admin_events.user_id = :id");
+    
+        $statement->bindParam(":id", $userId);
+    
+        $statement->execute();
+        $toReturn = $statement->fetchAll();
+    
+        echo json_encode($toReturn);
+    }
--- api/post.php
+++ api/post.php
@@ -1,5 +1,6 @@
 <?php
     require_once '../config/database.php';
+    require_once '../util/auth.php';
 
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
@@ -7,9 +8,43 @@
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization,X-Requested-With');
 
     // Instantiate DB & connect
-    // $database = new Database();
-    // $db = $database->connect();
+    $database = new Database();
+    $db = $database->connect();
 
-    $data = json_decode(file_get_contents("php://input"));
+    $headers = apache_request_headers();
+    if (!checkKey($headers)) {
+        echo "Invalid API key";
+        return;
+    }
 
-    echo json_encode($data);
+    $received = json_decode(file_get_contents("php://input"), true);
+    $data = $received["data"];
+    
+    switch ($received["action"]) {
+        case "EDIT_EVENT":
+            editEvent();
+            return;
+    }
+
+    function editEvent() {
+        global $data;
+        global $db;
+        $toReturn = "abcd";
+        $eventID = $data["eventID"];
+        $toEditArray = $data["toEditArray"];
+        try {
+            foreach ($toEditArray as $e) {
+                // update sql here
+                $attr = $e["attr"];
+                $statement = $db->prepare("UPDATE event SET " . $attr . "=:val WHERE event_id=:id");
+                $statement->bindParam(":val", $e["val"]);
+                $id = (int)$eventID;
+                $statement->bindValue(":id", $id, PDO::PARAM_INT);
+                $statement->execute();
+            }
+            echo $statement->rowCount() . " records UPDATED successfully";
+        }catch (PDOException $e) {
+            echo json_encode($e->getMessage());
+        }
+    }
+    
